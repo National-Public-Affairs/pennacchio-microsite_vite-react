2624dc4ab9f3ce944e4e40cf7d5c0f0311e66428
asb1512 <asb1512@gmail.com>
2023-03-22 16:44:24 -0400

    defining JSS styles

---
+++ src/useStyles.ts
@@ -0,0 +1,115 @@
+import { createUseStyles } from "react-jss";
+import heroImage from './assets/Mastrangelo.jpg';
+
+const useStyles = createUseStyles({
+  button: {
+    padding: '1rem',
+    border: 'none',
+    borderRadius: '1rem',
+    backgroundColor: 'var(--red)',
+    transition: 'background-color 150ms ease-in-out',
+    cursor: 'pointer',
+    '&:hover': {
+      backgroundColor: 'white',
+    },
+    '&:active': {
+      backgroundColor: 'gray',
+    },
+  },
+  heroButtonWrapper: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 6,
+    height: '20vh',
+    display: 'grid',
+    gridTemplate: '100% / 100%',
+  },
+  heroButton: {
+    justifySelf: 'center',
+    alignSelf: 'center',
+    width: '20vw',
+  },
+  heroTextWrapper: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 6,
+    display: 'grid',
+    gridTemplate: '100% / 100%',
+  },
+  heroText: {
+    justifySelf: 'right',
+    alignSelf: 'end',
+    width: '80%',
+    marginBottom: '12%',
+    color: 'white',
+    fontFamily: 'Roboto Mono',
+    fontSize: 'clamp(38px, 10vw, 150px)',
+    lineHeight: '8vw',
+    cursor: 'default',
+  },
+  heroImage: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    zIndex: 5,
+    width: '80%',
+    height: '70vh',
+    backgroundImage: `url(${heroImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundColor: 'black',
+    mixBlendMode: 'normal',
+    opacity: 0.7,
+  },
+  heroImageTwo: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    zIndex: 4,
+    width: '80%',
+    height: '70vh',
+    backgroundImage: `url(${heroImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundColor: 'black',
+    mixBlendMode: 'multiply'
+  },
+  recTwo: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    zIndex: 2,
+    width: '70%',
+    height: '75vh',
+    backgroundColor: '#DF2935',
+  },
+  recOne: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    zIndex: 1,
+    width: '88%',
+    height: '60vh',
+    backgroundColor: '#7C131A',
+  },
+  bg: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+  },
+  '@media only screen and (max-width: 900px) and (min-height: 300px)': {
+    heroText: {
+      marginBottom: '35%',
+    },
+  },
+});
+
+export default useStyles;
