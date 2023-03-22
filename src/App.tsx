f03fc90da09c97a7c5e4b04a7857f26fdb14f33e
asb1512 <asb1512@gmail.com>
2023-03-22 16:44:06 -0400

    rendering Intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Intro from './components/Intro';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      
+      <Intro />
     </div>
   );
 }
