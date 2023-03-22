9e3d02f61f8eba772c4bc9a7621950c9a1ed9611
asb1512 <asb1512@gmail.com>
2023-03-22 18:05:32 -0400

    handle animation trigger at app level

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Intro from './components/Intro';
+import Body from './components/Body';
 import './App.css';
 
 export default function App() {
+  const [display, setDisplay] = useState<boolean>(false);
+
+  useEffect(() => {
+    setDisplay(true);
+  }, []);
+  
   return (
     <div className="App">
-      <Intro />
+      <Intro display={display} setDisplay={setDisplay} />
+      <Body display={display} />
     </div>
   );
 }
