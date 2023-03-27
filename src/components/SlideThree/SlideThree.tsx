0cebef330d4f7897c91173afff116d118363bcf5
asb1512 <asb1512@gmail.com>
2023-03-27 12:24:14 -0400

    slide-specific styling

--- src/components/SlideThree/SlideThree.tsx
+++ src/components/SlideThree/SlideThree.tsx
@@ -1,11 +1,14 @@
 import { animated, useTransition } from '@react-spring/web';
-import audio from '../../assets/freeholder-tom-mastrangelo-threatening-and-cursing.mp3'
+import audio from '../../assets/freeholder-tom-mastrangelo-threatening-and-cursing.mp3';
+import { useStyles } from './styles';
 
 type Props = {
   currentSlide: number;
 }
 
 export default function SlideThree({ currentSlide }: Props) {
+  const styles = useStyles();
+
   const transitions = useTransition(currentSlide === 3, {
     from: {
       opacity: 0,
@@ -19,14 +22,15 @@ export default function SlideThree({ currentSlide }: Props) {
       opacity: 0,
       transform: 'translate(500px,-200px)',
     },
-    delay: 1000,
+    delay: 500,
   });
 
   return transitions((style, item) => (
     item && (
-      <animated.div style={style}>
-        <h2>
-          Don't take our word for it... just check out what Mastrangelo said about some of his Morris County colleagues.&nbsp;
+      <animated.div style={style} className={styles.wrapper}>
+        <h2 className={styles.text}>
+          Don't take our word for it... just check out what <span style={{ color: 'yellow' }}>Mastrangelo</span> said about some of his Morris County colleagues.&nbsp;
+          <br />
           <span style={{ color: 'var(--red)' }}>
             WARNING: EXTREMELY FOUL LANGUAGE
           </span>
@@ -35,6 +39,7 @@ export default function SlideThree({ currentSlide }: Props) {
         <audio
           controls
           src={audio}
+          className={styles.audio}
         >
           Listen to the clip!
         </audio>
---
+++ src/components/SlideThree/styles.ts
@@ -0,0 +1,24 @@
+import { createUseStyles } from 'react-jss';
+
+export const useStyles = createUseStyles({
+  wrapper: {
+    width: '75%',
+    maxWidth: 1200,
+    margin: 'auto',
+  },
+  text: {
+    fontSize: 'inherited',
+  },
+  audio: {
+    width: '100%',
+    maxWidth: 700,
+  },
+  '@media only screen and (max-width: 900px) and (min-height: 300px)': {
+    wrapper: {
+      width: '65%',
+    },
+    text: {
+      fontSize: '6vmin',
+    },
+  },
+});
