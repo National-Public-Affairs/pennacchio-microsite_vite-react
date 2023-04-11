679f031fff10edb5c3cc06816f5177ab30a4ddb0
asb1512 <asb1512@gmail.com>
2023-04-11 12:09:57 -0400

    added slide content

--- src/components/FinancialWoes/FinancialWoes.tsx
+++ src/components/FinancialWoes/FinancialWoes.tsx
@@ -29,7 +29,21 @@ export default function FinancialWoes({ currentSlide }: Props) {
   return transitions((style, item) => (
     item && (
       <animated.div style={style} className={`${styles.wrapper} ${generalStyles.body}`}>
+        <h2>
+          <span style={{ color: 'var(--red)' }}>Mastrangelo can't manage his own finances.</span> We can't trust him to manage ours!
+        </h2>
 
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            Mastrangelo and his businesses have been subject to 19 tax liens or civil judgements, totaling <span style={{ color: 'yellow' }}>more than $60,000</span>.
+          </li>
+          <li className={styles.listItem}>
+            Mastrangelo accepted a <span style={{ color: 'yellow' }}>$40,000 taxpayer-funded handout</span>.
+          </li>
+          <li className={styles.listItem}>
+            Don't believe Mastrangelo's lies. Under his watch, the Morris County budget has <span style={{ color: 'yellow' }}>increased taxes by more than $39 million</span>.
+          </li>
+        </ul>
       </animated.div>
     )
   ));
